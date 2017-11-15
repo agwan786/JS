07573193aa280e1923f908267777f529598bbedd
Altaf Husain <agwan786@yahoo.com>
2017-11-15 22:51:52 +0530

    update home component

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,10 +1,26 @@
 import { Component, OnInit } from '@angular/core';
+import { trigger,style,transition,animate,keyframes,query,stagger } from '@angular/animations';
 import {HomeModule} from './home.module';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.css'],
+  animations: [
+    trigger('goals', [
+      transition('* => *', [
+
+        query(':enter', style({ opacity: 0 }), {optional: true}),
+
+        query(':enter', stagger('300ms', [
+          animate('.6s ease-in', keyframes([
+            style({opacity: 0, transform: 'translateY(-75%)', offset: 0}),
+            style({opacity: .5, transform: 'translateY(35px)',  offset: 0.3}),
+            style({opacity: 1, transform: 'translateY(0)',     offset: 1.0}),
+          ]))]), {optional: true})
+      ])
+    ])
+  ]
 })
 
 export class HomeComponent implements OnInit {
@@ -13,7 +29,7 @@ export class HomeComponent implements OnInit {
   deleteBtn: string = 'Remove Contact';
   btnText: string = 'Add an Item';    // Add this line
   goalText: string = 'My first life goal';
-  goals = [];
+  goals = ['My first life goal', 'I want to climb a mountain', 'Go ice skiing'];
   contacts: Array<HomeModule>;
   error: string = '';
   constructor() {  	 
